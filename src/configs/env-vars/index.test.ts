f0b4c253037d52b6ad3bf6d861067bc3eeadc548
Emmanuel Onah <37575223+emmanuelonah@users.noreply.github.com>
2024-01-05 21:04:15 +0100

    Update: removed optional expression from test

--- src/configs/env-vars/index.test.ts
+++ src/configs/env-vars/index.test.ts
@@ -3,10 +3,7 @@ import { envVars } from './index.config';
 describe('envVars', () => {
     it('should return configuration object', () => {
         expect(typeof envVars.appName).toBe('string');
-        expect(typeof envVars.mongoDbUri).toBe('string');
         expect(typeof envVars.serverPort).toBe('number');
         expect(typeof envVars.serverUrl).toBe('string');
-        expect(typeof envVars.clientPort).toBe('number');
-        expect(typeof envVars.clientUrl).toBe('string');
     });
 });
